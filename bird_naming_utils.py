0d17ccc57ce4404a8715205666ef5969e8082356
Olly <56711484+Wologman@users.noreply.github.com>
2024-09-14 18:22:16 +1200

    Add files via upload

--- bird_naming_utils.py
+++ bird_naming_utils.py
@@ -26,6 +26,9 @@ class BirdNamer:
     def __init__(self, mapping_df):
         #modify this to ensure that the extra_name column of the mapping_df is filled
         self.bird_list = mapping_df['eBird'].tolist()
+        self.short_names_list = list(set(mapping_df['ExtraName']))
+        self.long_names_list = mapping_df['CommonName'].tolist()
+        self.science_names_list = mapping_df['ScientificName'].tolist()
         self.mapping_dict = mapping_df.set_index('eBird').apply(tuple, axis=1).to_dict()
          
     def vec_to_ebird(self, binary_array):
